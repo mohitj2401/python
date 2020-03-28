51dc0b856ce36b59c3dc636e01137fe8536fdb44
Mohit jain <jainm2600@gmail.com>
2020-03-28 22:17:05 +0530

    3rd Assignment

--- assignment 2/5.py
+++ assignment 2/5.py
@@ -1,3 +1,4 @@
+#uses of dictonary
 a={}
 for i in range(1,4):
     a[i]=input("Enter "+str(i)+" Word")
---
+++ assignment 3/1.py
@@ -0,0 +1,19 @@
+# Natural Number
+a=10
+for i in range(1,a+1):
+
+    print(i)
+# reverse order
+for i in range(0,a):
+    print(a-i)
+
+# 10 even Natural Number
+
+for i in range(1,11):
+    print(i*2)
+
+
+# 10 odd Natural Number
+
+for i in range(1,11):
+    print(2*i+1)
