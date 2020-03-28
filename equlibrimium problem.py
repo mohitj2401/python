77ee3e7404f99c9685c4bc4cc853ba401dd34da0
Mohit jain <jainm2600@gmail.com>
2020-03-28 15:52:33 +0530

    update sol

--- equlibrimium problem.py
+++ equlibrimium problem.py
@@ -1,23 +1,11 @@
-x=[3,1,2,4,8];
-
-
-def diff(a,b):
-    return a-b;
-
-
-def add( a ):
-    sum = 0
-    for x in a:
-        sum += x
-    return sum
+x=[3,1,2,4,3];
 
 aad={};
 l=len(x)
-# print(l)
 for s in range(1,l-1):
-    d=diff(sum(x[:s]),sum(x[s:l]));
-    aad[s]=abs(d)
+
+    aad[s]=abs(sum(x[:s])-sum(x[s:l]))
 drf=min(aad.values())
 for s in aad:
     if(aad[s]==drf):
-        print(str(x[s]) + " This is equilibrium point in the given list with diffis " + str(aad[s]))
+        print(str(x[s]) + " This is equilibrium point in the given list with diff. is " + str(aad[s]))
